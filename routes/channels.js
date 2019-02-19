66d2cd608861250d605e6b8d0eeede162e7df894
Chad Hill <chill@amobee.com>
2019-02-18 23:59:37 -0800

    changed input format to match slack proxy

--- routes/channels.js
+++ routes/channels.js
@@ -8,10 +8,7 @@ let router = express.Router();
 
 /* GET send a message to a channel */
 router.post('/send/', function( req, res, next ) {
-  let name = req.body.name;
-  let message = req.body.message;
-  let channel = req.body.channel;
-
+  let { name, message, channel } = JSON.parse(req.body.payload);
   gossip.channels.send( name, channel, message ).then( result => {
     logger.debug( result );
     res.send( 'SENT' );
